253195f9ac1b7f49ecf465b6167cbb986146cb6f
osechi-1111 <121627718+osechi-1111@users.noreply.github.com>
2023-11-26 13:41:14 +0900

    Add files via upload

---
+++ src/register.js
@@ -0,0 +1,8 @@
+function register(){
+    let month = document.getElementById("month").value;
+    let date = document.getElementById("date").value;
+    let day = document.getElementById("day").value;
+    console.log(month);
+    console.log(date);
+    console.log(day);
+  }
